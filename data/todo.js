64babf71bdd4b275ae69273b027de8fb189370ff
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-11-10 17:18:10 -0800

    first

---
+++ data/todo.js
@@ -0,0 +1,17 @@
+module.exports = [
+  TODO {
+    id: 1,
+    todo: 'wash the dishes',
+    completed: false,
+  },
+  TODO {
+    id: 2,
+    todo: 'take out the trash',
+    completed: false,
+  },
+  TODO {
+    id: 3,
+    todo: 'tidy your room',
+    completed: false,
+  }
+];
